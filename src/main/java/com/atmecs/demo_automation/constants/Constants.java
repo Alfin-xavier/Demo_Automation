3cb944eac60bcd90db1b58e8c5c51ff97b6f0b67
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-10-06 12:46:51 +0530

    Updated

--- src/main/java/com/atmecs/demo_automation/constants/Constants.java
+++ src/main/java/com/atmecs/demo_automation/constants/Constants.java
@@ -2,11 +2,20 @@ package com.atmecs.demo_automation.constants;
 
 public class Constants 
 {
-	public static final String USER_DIR = "webdriver.chrome.driver";
-	public static final String CHROME_PATH = "./lib/chromedriver.exe";
-	public static final String DATAS = "./src/main/resources/datas/testdatas.properties";
-	public static final String CONFIG = "./src/main/resources/datas/config.properties";
-	public static final String LOCATORS = "./src/main/resources/locators/locators.properties";
-	public static final String EXCEL_DATAS = "./src/main/resources/datas/Demo_Automation.xlsx";
-	public static final String SCREEN_SHOTS = "./screenshots";
+	/*
+	 * public static final String CHROME = "webdriver.chrome.driver"; public static
+	 * final String IE = "webdriver.ie.driver"; public static final String FIREFOX =
+	 * "webdriver.gecko.driver"; public static final String CHROME_PATH =
+	 * "./lib/chromedriver.exe"; public static final String IE_PATH =
+	 * "./lib/IEDriverServer.exe"; public static final String FIREFOX_PATH =
+	 * "./lib/geckodriver.exe";
+	 */
+		public static final String USER_DIR = "webdriver.chrome.driver";
+		public static final String CHROME_PATH = "./lib/chromedriver.exe";
+		public static final String DATAS = "./src/main/resources/datas/testdatas.properties";
+		public static final String CONFIG = "./src/main/resources/datas/config.properties";
+		public static final String LOCATORS = "./src/main/resources/locators/locators.properties";
+		public static final String EXCEL_DATAS = "./src/main/resources/datas/Demo_Automation.xlsx";
+		public static final String SCREEN_SHOTS = "./screenshots";
+	
 }
